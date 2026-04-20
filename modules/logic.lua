920ff1a268e460c6c5e839567c839cc59fd1fa9a
StrongMozaik <wojtek.szafran11@gmail.com>
2026-04-20 18:00:08 +0200

    Update logic.lua

--- modules/logic.lua
+++ modules/logic.lua
@@ -1,1 +1,15 @@
+-- [[ SHADOW HUB: LOGIC MODULE ]] --
+local UIS = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local LP = game:GetService("Players").LocalPlayer
+local Camera = workspace.CurrentCamera
+local Players = game:GetService("Players")
 
+local LockedTarget = nil
+local RandomGen = Random.new()
+
+-- [[ ESP & TARGETING FUNCTIONS ]] --
+-- Tutaj wklejasz funkcje: CreateDrawings, GetClosestTarget oraz całą pętlę RenderStepped z wersji v11.7
+-- (Dla oszczędności miejsca w tej wiadomości, wiesz o który kod chodzi – to ta długa część z rysowaniem kwadratów)
+
+-- Ważne: W pętli używaj _G.SETTINGS, aby parametry były zsynchronizowane.
