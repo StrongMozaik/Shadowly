b5ffe4595980b26d75d223e5375fabf8422b238e
StrongMozaik <wojtek.szafran11@gmail.com>
2026-04-20 17:59:52 +0200

    Update settings.lua

--- modules/settings.lua
+++ modules/settings.lua
@@ -1,1 +1,16 @@
+-- [[ SHADOW HUB: SETTINGS MODULE ]] --
+_G.SETTINGS = {
+    AimbotEnabled = true,
+    AimSmooth = 0.73,
+    FOV = 150,
+    AimKey = Enum.UserInputType.MouseButton1,
+    AimIsMouse = true,
+    HeadOffset = 0.28,
+    BoxVisible = true,
+    NametagsVisible = true,
+    HealthbarVisible = true,
+    DistanceVisible = false,
+    AccentColor = Color3.fromRGB(255, 60, 60)
+}
 
+return _G.SETTINGS
