d42980e826271d63e1741d2df2d8e8c385006a92
StrongMozaik <wojtek.szafran11@gmail.com>
2026-04-20 19:00:23 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,27 +1,18 @@
--- [[ SHADOW HUB: OFFICIAL LOADER ]] --
 local RawBase = "https://raw.githubusercontent.com/StrongMozaik/Shadowly/main/modules/"
 
 local function Load(module)
     local url = RawBase .. module .. ".lua"
     local success, source = pcall(function() return game:HttpGet(url) end)
-    
     if success then
-        local func, err = loadstring(source)
-        if func then
-            return func()
-        else
-            warn("Shadowly: Blad kompilacji " .. module .. ": " .. err)
-        end
-    else
-        warn("Shadowly: Nie znaleziono modulu " .. module)
+        local func = loadstring(source)
+        if func then return func() end
     end
 end
 
-print("Shadowly: Inicjalizacja...")
 Load("Settings")
 Load("Aimbot")
 Load("ESP")
 Load("SkeletonESP")
-Load("Tracers") -- NOWOŚĆ
+Load("Tracers")
+Load("Chams") -- Załadowanie neonowych rąk
 Load("Gui")
-print("Shadowly: Zaladowano wszystko!")
