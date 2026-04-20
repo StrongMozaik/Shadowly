76e1849a6fd70dea46d34ecaa4a87a06c6f4b8d3
StrongMozaik <wojtek.szafran11@gmail.com>
2026-04-20 20:12:26 +0200

    Update Settings.lua

--- modules/Settings.lua
+++ modules/Settings.lua
@@ -1,19 +1,18 @@
--- [[ SHADOW HUB: DEFAULT SETTINGS ]] --
 _G.SETTINGS = {
     AimbotEnabled = true,
-    AimSmooth = 0.85, -- Zwiększone wygładzenie
-    FOV = 95,         -- Zmniejszony zasięg koła celownika
+    AimSmooth = 0.85,
+    FOV = 95,
     AimKey = Enum.UserInputType.MouseButton1,
     AimIsMouse = true,
     HeadOffset = 0.28,
     BoxVisible = true,
-    SkeletonEnabled = false, -- Wyłączone na start
+    SkeletonEnabled = false,
     TracersEnabled = true,
     NametagsVisible = true,
     HealthbarVisible = true,
     DistanceVisible = false,
+    StretchedRes = false, -- DODANO
     AccentColor = Color3.fromRGB(255, 60, 60),
     MenuKey = Enum.KeyCode.Insert
 }
-
 return _G.SETTINGS
